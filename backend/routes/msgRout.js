478233c08f6685a9cdaecb3ec3451e0a79a00adf
nourhene <nourhenedhouibi9@gmail.com>
2025-03-05 23:31:21 +0100

    update

--- backend/routes/msgRout.js
+++ backend/routes/msgRout.js
@@ -1,15 +1,19 @@
-const Router = require('express').Router();
-const Controllers = require('../controllers/msgControllers');
-const{ protection}=require('../middlewares/auth_middlewares')
+import { Router } from 'express';  // Import Router from 'express'
+import * as Controllers from '../controllers/msgControllers';  // Import everything from msgControllers
+import { protection } from '../middlewares/auth_middlewares';  // Import protection middleware
+
+const Router = Router();
 
 // POST route to send a message
-Router.get('/hello',(req, res)=>{
-    return res.status(200).json({message:"hello from message route"})
-})
-Router.get('/:id', protection,Controllers.getMsgs); 
+Router.get('/hello', (req, res) => {
+  return res.status(200).json({ message: "hello from message route" });
+});
+
+Router.get('/:id', protection, Controllers.getMsgs); 
 Router.post('/send/:user_id', protection, Controllers.sendMessage);
 
-Router.put('/update/:msg_id',protection,Controllers.updateMsgs)
-Router.delete('/delete/:msg_id',protection,Controllers.DeleteMessage)
+Router.put('/update/:msg_id', protection, Controllers.updateMsgs);
+Router.delete('/delete/:msg_id', protection, Controllers.DeleteMessage);
+
 // Export Router
-module.exports = Router;
+export default Router;
