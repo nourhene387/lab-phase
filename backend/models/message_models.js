5fb9ab44d35384fb36d6521a2c274addad189c23
nourhene <nourhenedhouibi9@gmail.com>
2025-03-06 00:58:12 +0100

    update

--- backend/models/message_models.js
+++ backend/models/message_models.js
@@ -1,30 +1,31 @@
-const mongoose= require('mongoose');
+// /src/backend/models/message_models.js
 
-/********* */
-// create message schema
-const messageSchema=new mongoose.Schema({
-    senderId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User',
-        required:true
+const mongoose = require('mongoose');
+
+// Create message schema
+const messageSchema = new mongoose.Schema({
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User', // Assuming you have a 'User' model
+        required: true
     },
-    receiverId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User',
-        required:true
+    receiverId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User', // Assuming you have a 'User' model
+        required: true
     },
-    text:{
-        type:String,
-        
+    text: {
+        type: String,
+        required: true
     },
-    image:{
-        type:String,
+    image: {
+        type: String,
     },
-   
-    date:{
-        type:Date,
-        default:Date.now
+    date: {
+        type: Date,
+        default: Date.now
     }
+});
 
-})
-module .exports=mongoose.model('Messages',messageSchema)
+// Create and export the Message model
+module.exports = mongoose.model('Message', messageSchema);
