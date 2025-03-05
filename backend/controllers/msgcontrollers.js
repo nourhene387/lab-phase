8010d3e64d3e650fac4131c68269d0996c3aecde
nourhene <nourhenedhouibi9@gmail.com>
2025-03-05 23:33:20 +0100

    update

--- backend/controllers/msgcontrollers.js
+++ backend/controllers/msgcontrollers.js
@@ -1,83 +1,84 @@
-const Message=require('../models/message_models')
-const User=require('../models/user_model')
-const {getReceiverSocketId }=require('../db/socket')
- const io =require('../db/socket')
-// post massage by sender id
-exports.sendMessage = async (req, res) => {
-    try {
-      const { text,image } = req.body;
-      const { user_id: receiverId } = req.params;
-      const senderId = req.user._id;
-     
- 
-      const newMessage = new Message({
-        senderId,
-        receiverId,
-        text,
-        image
-       
-      });
-  
-      await newMessage.save();
-  
-      const receiverSocketId = getReceiverSocketId(receiverId);
-      if (receiverSocketId) {
-        io.to(receiverSocketId).emit("newMessage", newMessage);
-      }
-  
-      res.status(201).json(newMessage);
-    } catch (error) {
-      console.log("Error in sendMessage controller: ", error.message);
-      res.status(500).json({ error: "Internal server error" });
-    }
-  };
-  
-// get all massages by sender id
-exports.getMsgs=async(req,res)=>{
-    try{
-         const {id: userTochatId}=req.params;
-         const myId=req.user._id;
-         const messages=await Message.find({
-             $or:[
-                 {senderId:myId,receiverId:userTochatId},
-                 {senderId:userTochatId,receiverId:myId}
-             ],
-         })
-        return res.status(200).json(messages)
-         
+import Message from '../models/message_models';
+import User from '../models/user_model';
+import { getReceiverSocketId } from '../db/socket';
+import { io } from '../db/socket';
 
-    }catch(err){
-        console.log(err)
-        return res.status(500).json({message:"Server Error",err})
+// Post message by sender id
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { user_id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image
+    });
+
+    await newMessage.save();
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
     }
-}
-// update message by id
-exports.updateMsgs=async (req,res)=>{
-  try{
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// Get all messages by sender id
+export const getMsgs = async (req, res) => {
+  try {
+    const { id: userTochatId } = req.params;
+    const myId = req.user._id;
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userTochatId },
+        { senderId: userTochatId, receiverId: myId }
+      ],
+    });
+    return res.status(200).json(messages);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Server Error", err });
+  }
+};
+
+// Update message by id
+export const updateMsgs = async (req, res) => {
+  try {
     const { id } = req.user;
     console.log("userauth:", id);
-    const {msg_id}=req.params;
-    const newtext=req.body.text
-    const msg=await Message.findById(msg_id);
-    if(!msg){
-      return res.status(404).json({message:"Message not found"})
-    } 
-     console.log(msg.senderId)
-     if (msg.senderId.toString() === id) {
-   // msg.text=newtext
-   msg.text=newtext;
-    console.log(msg.text)
-    await msg.save()
-    return res.status(200).json({message:'Message updated',msg})
+    const { msg_id } = req.params;
+    const newText = req.body.text;
+    const msg = await Message.findById(msg_id);
+
+    if (!msg) {
+      return res.status(404).json({ message: "Message not found" });
     }
-    return res.status(400).json({message:'you can not update the message',msg})
-  }catch(err){
- console.log(err)
-        return res.status(500).json({message:"Server Error",err})
+
+    console.log(msg.senderId);
+    if (msg.senderId.toString() === id) {
+      msg.text = newText;
+      console.log(msg.text);
+      await msg.save();
+      return res.status(200).json({ message: 'Message updated', msg });
+    }
+
+    return res.status(400).json({ message: 'You cannot update the message', msg });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Server Error", err });
   }
-}
-// delete message by id
-exports.DeleteMessage = async (req, res) => {
+};
+
+// Delete message by id
+export const DeleteMessage = async (req, res) => {
   try {
     const { msg_id } = req.params;
 
@@ -89,10 +90,8 @@ exports.DeleteMessage = async (req, res) => {
     }
 
     return res.status(200).json({ message: 'Message deleted', msg });
-
   } catch (err) {
     console.log(err);
     return res.status(500).json({ message: "Server Error", err });
   }
 };
-
