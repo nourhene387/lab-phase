abf187a42b0815565d9c17d89e7333b6f7520d98
nourhene <nourhenedhouibi9@gmail.com>
2025-03-06 02:20:06 +0100

    update

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,17 +1,21 @@
-const mongoose=require('mongoose');
-require('dotenv').config();
-MONGODB_URI=process.env.MongoDB_URI;
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-//connect database
- const connectDB= async()=>{
-    try{
-        await mongoose.connect(MONGODB_URI)
+// Load environment variables from .env file
+dotenv.config();
+
+// Get the MongoDB URI from environment variables
+const MONGODB_URI = process.env.MONGO_URI;  // Ensure the environment variable is correctly set
+
+// Connect to the database
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('Database connection successful');
-
-    }catch(err){
+    } catch (err) {
         console.error('Database connection error', err);
-  process.exit(1);
-
+        process.exit(1); // Exit the process with failure code
     }
- }
-module.exports=connectDB
+};
+
+export default connectDB;
