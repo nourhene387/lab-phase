05cedc748e25ba2081dff9c5d2025a15da1fb14b
nourhene <nourhenedhouibi9@gmail.com>
2025-03-06 01:24:38 +0100

    update

--- backend/models/message_models.js
+++ backend/models/message_models.js
@@ -1,31 +1,31 @@
 // /src/backend/models/message_models.js
 
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';  // Use ES6 import syntax for mongoose
 
 // Create message schema
 const messageSchema = new mongoose.Schema({
-    senderId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User', // Assuming you have a 'User' model
-        required: true
-    },
-    receiverId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User', // Assuming you have a 'User' model
-        required: true
-    },
-    text: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: String,
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // Assuming you have a 'User' model
+    required: true
+  },
+  receiverId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // Assuming you have a 'User' model
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-// Create and export the Message model
-module.exports = mongoose.model('Message', messageSchema);
+// Create and export the Message model using default export
+export default mongoose.model('Message', messageSchema);
